25452e22acd932feb33620c8cdc7c54f9717c167
SHINING <jack7770126@126.com>
2017-09-04 10:29:21 +0800

    [new function] finish the lesson 04.

--- 01/app.js
+++ 01/app.js
@@ -5,7 +5,7 @@ var utility = require('utility');
 //建立 express 实例
 var app = express();
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     //从 req.query 中取出我们的 q 参数。
     //如果是 post 传来的 body 数据，则是在 req.body 里面，不过 express 默认不处理 body 中的信息，需要引入 body-parser 这个中间件
     //如果分不清什么是 query， 什么是 body 的话，那就要补充一下 http 的知识了。
@@ -18,6 +18,6 @@ app.get('/', function(req, res){
     res.send(md5Value);
 });
 
-app.listen(3000, function(req, res){
+app.listen(3000, function (req, res) {
     console.log('app is running at port 3000...');
 });
--- 03/app.js
+++ 03/app.js
@@ -9,31 +9,60 @@ var url = require('url');
 var app = express();
 var cnodeUrl = 'https://cnodejs.org/';
 
-app.get('/', function(req, res, next){
-    superagent.get(cnodeUrl)
-	.end(function(err, sres){
-	    if(err){
-	        return next(err);
- 	    }
-	    console.log(sres);
-	    var topicUrls = [];
-	    var $ = cheerio.load(sres.text);
+app.get('/', function (req, res, next) {
+	superagent.get(cnodeUrl)
+		.end(function (err, sres) {
+			if (err) {
+				return next(err);
+			}
+			var topicUrls = [];
+			var $ = cheerio.load(sres.text);
 
-	    //获取首页所有链接
-	    $('#topic_list .topic_title').each(function(index, element){
-		var $element = $(element);
+			//获取首页所有链接
+			$('#topic_list .topic_title').each(function (index, element) {
+				var $element = $(element);
 
-		//$elememt.attr('href') 本来的样子是 /topic/5ac21b412323d21
-		//我们用 url.resolve 来自动推断出完整 url ，变成https://...
-		var href = url.resolve(cnodeUrl, $element.attr('href'));
-		topicUrls.push(href);
-	    });
-	    console.log(topicUrls);
-	    res.send(topicUrls);
+				//$elememt.attr('href') 本来的样子是 /topic/5ac21b412323d21
+				//我们用 url.resolve 来自动推断出完整 url ，变成https://...
+				var href = url.resolve(cnodeUrl, $element.attr('href'));
+				topicUrls.push(href);
+			});
 
-	});
+			//得到一个 eventproxy 的实例
+			var ep = new eventproxy();
+
+			//命令 ep 重复监听 topicUrls.length 次（这里是40次） `topic_html` 事件再行动
+			ep.after('topic_html', topicUrls.length, function (topics) {
+				//topics 是个数组，包含了 40 次 ep.emit('topic_html', pair) 中的那40个 pair
+
+				//开始行动
+				topics = topics.map(function (topicPair) {
+					//jquery 用法
+					var topicUrl = topicPair[0];
+					var topicHtml = topicPair[1];
+					var $ = cheerio.load(topicHtml);
+					return ({
+						title: $('.topic_full_title').text().trim(),
+						href: topicUrl,
+						comment1: $('.reply_content').eq(0).text().trim(),
+					});
+				});
+
+				console.log('final:');
+				console.log(topics);
+			});
+
+			topicUrls.forEach(function (topicUrl) {
+				superagent.get(topicUrl)
+					.end(function (err, res) {
+						console.log('fetch ' + topicUrl + 'successful');
+						ep.emit('topic_html', [topicUrl, res.text]);
+					});
+			});
+
+		});
 });
 
-app.listen(3000, function(){
-	console.log('start...');
+app.listen(3000, function () {
+	console.log('starting listening port 3000 ...');
 })
